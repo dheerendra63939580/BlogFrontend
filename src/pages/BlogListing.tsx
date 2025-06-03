68c5d2cdc11e2e8ef3ec9d9d9f984a4955c32e47
dheerendra kumar <dheerendra16kumar@gmail.com>
2025-06-04 01:18:58 +0530

    feat: add loading when getting all blogs

--- src/pages/BlogListing.tsx
+++ src/pages/BlogListing.tsx
@@ -2,14 +2,15 @@ import { useQuery } from "@tanstack/react-query"
 import { getAllBlogs } from "../api"
 import type { BlogResponse } from "../types/types"
 import BlogCard from '../components/BlogCard'
+import { PageLoading } from "../components/PageLoading";
 
 function BlogListing() {
 
-  const { data } = useQuery<BlogResponse, Error>({
+  const { data, isPending } = useQuery<BlogResponse, Error>({
     queryKey: ["allBlogs"],
     queryFn: () => getAllBlogs(),
   });
-
+  if(isPending) return <PageLoading />
   return (
     <div className="grid grid-cols-1 md:grid-cols-2 gap-(--gap)">
       {data?.data.blogs.map((blogInfo) => (
